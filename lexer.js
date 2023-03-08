8e49b5f289a5376c089bfd415d7f2250caef4734
Cassiano Nakaoka <72208208+casnaka@users.noreply.github.com>
2023-03-08 01:08:05 +0000

    ADD THE LAST

--- lexer.js
+++ lexer.js
@@ -1,7 +1,5 @@
 const fs = require('fs')
 
-const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
 const blanks = [' ', '\t', '\n', '\r']
 const symbolsTable = []
 
@@ -12,12 +10,12 @@ const openFile = () => {
   const filename = process.argv[2]
 
   // Se houver o terceiro parâmetro
-  if(filename) {
+  if (filename) {
     try {
       const fileContent = fs.readFileSync(filename, 'utf-8')
       return fileContent
     }
-    catch(error) {
+    catch (error) {
       console.error(error)
       process.exit(-1)
     }
@@ -30,78 +28,96 @@ const openFile = () => {
 }
 
 const terminate = finalState => {
-  if(! blanks.includes(char)) lexeme += char 
+  if (!blanks.includes(char)) lexeme += char
   state = finalState
-  switch(finalState){
-      case 6.1: //plus
-      symbolsTable.append({
-          lexeme,
-          token: 'plus'
+  switch (finalState) {
+    case 6.1: //plus
+      symbolsTable.push({
+        lexeme,
+        token: 'plus'
       })
       break
   }
 
-  switch(finalState){
-      case 6.2: //minus
-      symbolsTable.append({
-          lexeme,
-          token: 'minus'
+  switch (finalState) {
+    case 6.2: //minus
+      symbolsTable.push({
+        lexeme,
+        token: 'minus'
       })
       break
   }
 
-  switch(finalState){
-      case 6.3: //times
-      symbolsTable.append({
-          lexeme,
-          token: 'times'
+  switch (finalState) {
+    case 6.3: //times
+      symbolsTable.push({
+        lexeme,
+        token: 'times'
       })
       break
   }
 
-  switch(finalState){
-      case 6.4: //div
-      symbolsTable.append({
-          lexeme,
-          token: 'div'
+  switch (finalState) {
+    case 6.4: //div
+      symbolsTable.push({
+        lexeme,
+        token: 'div'
       })
       break
   }
 
-  switch(finalState){
-      case 6.5: //lparen
-      symbolsTable.append({
-          lexeme,
-          token: 'lparen'
+  switch (finalState) {
+    case 6.5: //lparen
+      symbolsTable.push({
+        lexeme,
+        token: 'lparen'
       })
       break
   }
 
-  switch(finalState){
-      case 6.6: //rparen
-      symbolsTable.append({
-          lexeme,
-          token: 'rparen'
+  switch (finalState) {
+    case 6.6: //rparen
+      symbolsTable.push({
+        lexeme,
+        token: 'rparen'
       })
       break
   }
 
-  switch(finalState){
-      case 6.7: //rparen
-      symbolsTable.append({
-          lexeme,
-          token: 'keyword',
-          value: lexame
+  switch (finalState) {
+    case 6.7: //keyword
+      symbolsTable.push({
+        lexeme,
+        token: 'keyword',
+        value: lexame
       })
       break
   }
 
-  switch(finalState){
-      case 6.8: //rparen
-      symbolsTable.append({
-          lexeme,
-          token: 'identifier',
-          value: lexame
+  switch (finalState) {
+    case 6.8: //identifier
+      symbolsTable.push({
+        lexeme,
+        token: 'identifier',
+        value: lexame
+      })
+      break
+  }
+  switch (finalState) {
+    case 6.9: //number
+      symbolsTable.push({
+        lexeme,
+        token: 'number',
+        value: lexame
+      })
+      break
+  }
+
+  switch (finalState) {
+    case 6.10: //assing
+      symbolsTable.push({
+        lexeme,
+        token: 'assing'
       })
       break
   }
@@ -110,22 +126,27 @@ const terminate = finalState => {
   lexeme = ''
 }
 
+const displayError = () => {
+  console.error(`ERROR: unexpected char ${char} at ${pos} (state ${state})`)
+  process.exit(1)
+}
+
 const analyze = source => {
   let state = 0
 
   // Percorre todo o código-fonte, caractere a caractere
-  for(let pos = 0; pos < source.length; pos++) {
+  for (let pos = 0; pos < source.length; pos++) {
 
     // Lê um caractere do código-fonte
     const char = source.charAt(pos)
 
-    switch(state) {
+    switch (state) {
       case 0:
-        
-        if (char ==='r') advanceTo(1)
+
+        if (char === 'r') advanceTo(1)
         else if (char === 'w') advanceTo(7)
-        else if (digits.includes(char)) advanceTo(13)
-        else if (char === '.') advanceTo(15)
+        else if (char.match(/0-9/)) advanceTo(13)
+        else if (char === '.') advanceTo(14)
         else if (char === ':') advanceTo(17)
         else if (char.match(/a-zA-Z/)) advanceTo(5)
         else if (char === '+') advanceTo(6.1)
@@ -137,38 +158,84 @@ const analyze = source => {
 
         else if (blanks.includes(char)) continue
 
-            else console.error(`ERROR: unexpected char ${char} at ${pos}`)
+        else displayError()
 
-            break
-        case 1:
-            if(char === 'e') advanceTo(2)
-            else if (char.match(/a-zA-Z0-9/)) advanceTo(5) 
-            else console.error(`ERROR: unexpected char ${char} at ${pos}`)
-            break 
-        case 2:
-            if(char === 'a') advanceTo(3)
-            else if (char.match(/a-zA-Z0-9/)) advanceTo(5) 
-            else console.error(`ERROR: unexpected char ${char} at ${pos}`)
-            break 
-        case 3:
-            if(char === 'd') advanceTo(4)
-            else if (char.match(/a-zA-Z0-9/)) advanceTo(5) 
-            else console.error(`ERROR: unexpected char ${char} at ${pos}`)
-            break
+        break
+      case 1:
+        if (char === 'e') advanceTo(2)
+        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else displayError()
+        break
+      case 2:
+        if (char === 'a') advanceTo(3)
+        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else displayError()
+        break
+      case 3:
+        if (char === 'd') advanceTo(4)
+        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else displayError()
+        break
 
-        case 4:
-            if(char.match(/a-zA-Z0-9/)) advanceTo(5)
-            else if (blanks.includes(char)) terminate(6.7) 
-            else console.error(`ERROR: unexpected char ${char} at ${pos}`)
-            break
+      case 4:
+        if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (blanks.includes(char)) terminate(6.7)
+        else displayError()
+        break
 
-        case 5:
-            if(char.match(/a-zA-Z0-9/)) advanceTo(5)
-            else if (blanks.includes(char)) terminate(6.8) 
-            else console.error(`ERROR: unexpected char ${char} at ${pos}`)
-            break
+      case 5:
+        if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (blanks.includes(char)) terminate(6.8)
+        else displayError()
+        break
+      case 7:
+        if (char === 'r') advanceTo(8)
+        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (blanks.includes(char)) terminate(6.8)
+        else displayError()
+        break
+      case 8:
+        if (char === 'i') advanceTo(8)
+        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (blanks.includes(char)) terminate(6.8)
+        else displayError()
+        break
+      case 9:
+        if (char === 't') advanceTo(8)
+        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (blanks.includes(char)) terminate(6.8)
+        else displayError()
+        break
+      case 10:
+        if (char === 'e') advanceTo(8)
+        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (blanks.includes(char)) terminate(6.8)
+        else displayError()
+        break
+      case 11:
+        if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (blanks.includes(char)) terminate(6.7)
+        else displayError()
+        break
+      case 13:
+        if (char.match(/0-9/)) advanceTo(13)
+        else if (char === '.') advanceTo(14)
+        else if (blanks.includes(char)) terminate(6.9)
+        else displayError()
+        break
+      case 14:
+        if (char.match(/0-9/)) advanceTo(14)
+        else if (blanks.includes(char)) terminate(6.9)
+        else displayError()
+        break
+      case 17:
+        if (char === '=') advanceTo(6.10)
+        else displayError()
+        break
     }
   }
+  console.log('------------------------simbolos-----------------------------')
+  console.log(symbolsTable)
 }
 
 const source = openFile()
