25f1641be7eb8017077f69e354e5ba1cafa759e7
Cassiano Nakaoka <72208208+casnaka@users.noreply.github.com>
2023-03-15 00:02:48 +0000

    change

--- lexer.js
+++ lexer.js
@@ -114,7 +114,7 @@ const terminate = finalState => {
   }
 
   switch (finalState) {
-    case 6.10: //assing
+    case 6.11: //assing
       symbolsTable.push({
         lexeme,
         token: 'assing'
@@ -127,12 +127,18 @@ const terminate = finalState => {
 }
 
 const displayError = () => {
-  console.error(`ERROR: unexpected char ${char} at ${pos} (state ${state})`)
-  process.exit(1)
+  console.error(`ERROR[${row}:${col}]: unexpected char "${char}" (state ${state})`)
+  process.exit(-1)
 }
 
 const analyze = source => {
   let state = 0
+  let lexame = ''
+  let char = ''
+  let pos 
+  let row = 1
+  let col = 1
+  let symbolsTable = []
 
   // Percorre todo o código-fonte, caractere a caractere
   for (let pos = 0; pos < source.length; pos++) {
@@ -140,12 +146,17 @@ const analyze = source => {
     // Lê um caractere do código-fonte
     const char = source.charAt(pos)
 
+    if(char = '\n'){
+      row++
+      col = 1
+    }
+
     switch (state) {
       case 0:
 
         if (char === 'r') advanceTo(1)
         else if (char === 'w') advanceTo(7)
-        else if (char.match(/0-9/)) advanceTo(13)
+        else if (char.match(regexDigits)) advanceTo(13)
         else if (char === '.') advanceTo(14)
         else if (char === ':') advanceTo(17)
         else if (char.match(/a-zA-Z/)) advanceTo(5)
@@ -163,76 +174,78 @@ const analyze = source => {
         break
       case 1:
         if (char === 'e') advanceTo(2)
-        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (char.match(regexAlphanum)) advanceTo(5)
         else displayError()
         break
       case 2:
         if (char === 'a') advanceTo(3)
-        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (char.match(regexAlphanum)) advanceTo(5)
         else displayError()
         break
       case 3:
         if (char === 'd') advanceTo(4)
-        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (char.match(regexAlphanum)) advanceTo(5)
         else displayError()
         break
 
       case 4:
-        if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        if (char.match(regexAlphanum)) advanceTo(5)
         else if (blanks.includes(char)) terminate(6.7)
         else displayError()
         break
 
       case 5:
-        if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        if (char.match(regexAlphanum)) advanceTo(5)
         else if (blanks.includes(char)) terminate(6.8)
         else displayError()
         break
       case 7:
         if (char === 'r') advanceTo(8)
-        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (char.match(regexAlphanum)) advanceTo(5)
         else if (blanks.includes(char)) terminate(6.8)
         else displayError()
         break
       case 8:
         if (char === 'i') advanceTo(8)
-        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (char.match(regexAlphanum)) advanceTo(5)
         else if (blanks.includes(char)) terminate(6.8)
         else displayError()
         break
       case 9:
         if (char === 't') advanceTo(8)
-        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (char.match(regexAlphanum)) advanceTo(5)
         else if (blanks.includes(char)) terminate(6.8)
         else displayError()
         break
       case 10:
         if (char === 'e') advanceTo(8)
-        else if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        else if (char.match(regexAlphanum)) advanceTo(5)
         else if (blanks.includes(char)) terminate(6.8)
         else displayError()
         break
       case 11:
-        if (char.match(/a-zA-Z0-9/)) advanceTo(5)
+        if (char.match(regexAlphanum)) advanceTo(5)
         else if (blanks.includes(char)) terminate(6.7)
         else displayError()
         break
       case 13:
-        if (char.match(/0-9/)) advanceTo(13)
+        if (char.match(regexDigits)) advanceTo(13)
         else if (char === '.') advanceTo(14)
         else if (blanks.includes(char)) terminate(6.9)
         else displayError()
         break
       case 14:
-        if (char.match(/0-9/)) advanceTo(14)
+        if (char.match(regexDigits)) advanceTo(14)
         else if (blanks.includes(char)) terminate(6.9)
         else displayError()
         break
       case 17:
-        if (char === '=') advanceTo(6.10)
+        if (char === '=') advanceTo(6.11)
         else displayError()
         break
     }
+
+    if (char !== '\r') col++
   }
   console.log('------------------------simbolos-----------------------------')
   console.log(symbolsTable)
